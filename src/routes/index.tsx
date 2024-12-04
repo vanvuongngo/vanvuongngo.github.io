2bd5c3a3186794fbf87064f9d8a0b113c950e567
Van Vuong Ngo <20791239+vanvuongngo@users.noreply.github.com>
2024-12-04 19:27:24 +0100

    init qwik

---
+++ src/routes/index.tsx
@@ -0,0 +1,24 @@
+import { component$ } from "@builder.io/qwik";
+import type { DocumentHead } from "@builder.io/qwik-city";
+
+export default component$(() => {
+  return (
+    <>
+      <h1>Van Vuong Ngo - Projects in 2025</h1>
+      <p>
+        Hi, thanks for your interest. You can find me on{" "}
+        <a href="https://www.linkedin.com/in/vanvuong-ngo/">linkedIn</a>
+      </p>
+    </>
+  );
+});
+
+export const head: DocumentHead = {
+  title: "Van Vuong Ngo - Projects in 2025",
+  meta: [
+    {
+      name: "description",
+      content: "Van Vuong Ngo - Projects in 2025",
+    },
+  ],
+};
